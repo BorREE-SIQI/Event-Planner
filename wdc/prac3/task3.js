74d83062dbb0dcbcf0c2f124e310bca34e5dea3b
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-03-23 03:11:02 +0000

    Wed, Mar 23, 2022, 3:11 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -9,11 +9,10 @@ function count()
 function function1()
 {
     const n = new Date();
-    //const date = document.createElement("post-time")
-    // const a = document.getElementsByClassName("post-time");
-    document.getElementById("posts").innerText = n;
+    const a = document.getElementsByClassName("post-time");
+    a[0].innerText = n;
 
     var x = document.getElementById("t").value;
-    var a= document.getElementsByClassName("post-content");
-    a[0].innerHTML+= x;
+    const b = document.getElementsByClassName("post-content");
+    b[0].innerHTML += x;
 }
