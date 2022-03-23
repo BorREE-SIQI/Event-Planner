d45ebdac59fc5446db1388fce5309f6c5a797956
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-03-23 02:01:07 +0000

    Wed, Mar 23, 2022, 2:01 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -11,4 +11,8 @@ function function1()
     const n = new Date();
     const a = document.getElementsByClassName("post-time");
     a[0].innerText = n;
+
+    var x = document.getElementById("t").value;
+    const b = document.getElementsByClassName("post-content");
+    b[0].innerHTML = x;
 }
