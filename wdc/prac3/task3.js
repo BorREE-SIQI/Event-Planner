1ff6fc3ed4ab3d00dd53017cc845b15f1ec3e4dd
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-03-23 05:10:17 +0000

    Wed, Mar 23, 2022, 5:10 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -8,27 +8,6 @@ function count()
 
 function function1()
 {
-    // const n = new Date();
-    // var posts=[]
-    // const date= document.getElementsByClassName("post-time");
-    // var x = document.getElementById("t").value;
-    // const content= document.getElementsByClassName("post-content");
-    // posts.push({
-    //     "date": date,
-    //     "content": content
-
-    // })
-    // for(var i=0;i<posts.length;i++){
-    //     a[0].innerText += n;
-    //     b[0].innerHTML += x;
-    // }
-    //const a = document.getElementsByClassName("post-time");
-    //a[0].innerText += n;
-
-    //var x = document.getElementById("t").value;
-    //const b = document.getElementsByClassName("post-content");
-    //b[0].innerHTML += x;
-
     const n = new Date();
     const a = document.createElement("span");
     a.className = "post-time";
@@ -41,4 +20,9 @@ function function1()
     b.innerHTML = x;
     document.getElementById("posts").appendChild(b);
 
+}
+
+function hide()
+{
+    
 }
