f3cfee1ff512a7d2492c8f5bf92f8d9a5297b326
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-03-23 03:35:36 +0000

    Wed, Mar 23, 2022, 3:35 AM +00:00

--- wdc/prac3/task3.js
+++ wdc/prac3/task3.js
@@ -11,13 +11,16 @@ function function1()
     const n = new Date();
     var posts=[]
     var date= document.getElementsByClassName("post-time");
-    
+    var content= document.getElementsByClassName("post-content");
     posts.push({
         "date": date,
         "content": content
 
     })
-    for()
+    for(var i=0;i<posts.length;i++){
+        a[0].innerText += n;
+        b[0].innerHTML += x;
+    }
     const a = document.getElementsByClassName("post-time");
     a[0].innerText += n;
 
