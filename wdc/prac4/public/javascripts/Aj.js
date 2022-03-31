db259308694ce1f89f98db5cd4a3dab492791d68
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-03-31 09:27:30 +0000

    Thu, Mar 31, 2022, 9:27 AM +00:00

--- wdc/prac4/public/javascripts/Aj.js
+++ wdc/prac4/public/javascripts/Aj.js
@@ -1,10 +1,8 @@
-var responseText = "This page was last viewed "+String(new Date());
-
 function load() {
     var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("para").innerHTML = this.responseText;
+        document.getElementById("para").innerHTML = "This page was last viewed "+this.responseText;
     }
   };
     xhttp.open("GET", "/last.txt");
