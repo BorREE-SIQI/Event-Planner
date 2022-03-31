6407b5b352dfb41451692cdbe3c198a8a33e5079
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-03-31 00:29:26 +0000

    Thu, Mar 31, 2022, 12:29 AM +00:00

--- wdc/prac4/routes/index.js
+++ wdc/prac4/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
-var date
+var date;
 
 router.get('/last.txt', function(req, res, next) {
   res.send(date);
