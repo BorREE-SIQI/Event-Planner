616357048cc6d384fe4dac4ccc6d76406fb4b486
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-04-06 04:14:45 +0000

    Wed, Apr 6, 2022, 4:14 AM +00:00

--- wdc/prac5/task2.2.sql
+++ wdc/prac5/task2.2.sql
@@ -1,3 +1,23 @@
 USE enrolment;
 
-CREATE TABLE Students();
+CREATE TABLE Students(
+    student_id INT,
+    given_name VARCHAR(63),
+    family_name VARCHAR(63),
+    program VARCHAR(255),
+    PRIMARY KEY (student_id)
+);
+
+CREATE TABLE Subjects(
+    subject_code INT,
+    subject VARCHAR(127),
+    faculty VARCHAR(63),
+    PRIMARY KEY (subject_code)
+);
+
+CREATE TABLE Enrolments(
+    student_id INT,
+    subject_code INT,
+    mark INT,
+    PRIMARY KEY (subject_code)
+);
