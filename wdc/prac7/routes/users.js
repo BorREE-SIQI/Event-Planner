939552e67ae5790b297f20b67671dd581d98b23b
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-18 03:32:32 +0000

    Wed, May 18, 2022, 3:32 AM +00:00

--- wdc/prac7/routes/users.js
+++ wdc/prac7/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/users', function(req, res, next) {
+router.post('/addpost', function(req, res, next) {
   console.log("POST from a user");
   next();
 });
