0e03015b4da52d2b44c445b74db24404df81f4d1
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-18 00:19:06 +0000

    Wed, May 18, 2022, 12:19 AM +00:00

--- wdc/prac7/routes/index.js
+++ wdc/prac7/routes/index.js
@@ -5,6 +5,13 @@ var router = express.Router();
 router.get('/brew', function(req, res, next) {
   var d = req.query.drink;
   if(d == "tea")
+  {
+    res.send('A delicious cup of tea!');
+  }
+  else if(d == 'coffee')
+  {
+    res.send(418);
+  }
 });
 
 module.exports = router;
