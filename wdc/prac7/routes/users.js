1b76cadd2e000fe19f29ad9e1c2b31ca83daedaf
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-18 03:23:29 +0000

    Wed, May 18, 2022, 3:23 AM +00:00

--- wdc/prac7/routes/users.js
+++ wdc/prac7/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/users', function(req, res, next) {
+  console.log("POST from a user");
+  next();
 });
 
 module.exports = router;
