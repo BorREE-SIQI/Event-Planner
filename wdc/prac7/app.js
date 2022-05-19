81129ccea2efa14bc3a5d328d4b20edb35bc6c4e
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-19 00:33:30 +0000

    Thu, May 19, 2022, 12:33 AM +00:00

--- wdc/prac7/app.js
+++ wdc/prac7/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var session = require('express-session');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -18,14 +17,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(session({                             //           //
-  secret: '7777777ssssqqq',          //           //
-  resave: false,                              // THIS CODE //
-  saveUninitialized: true,                    //           //
-  cookie: { secure: false }                   //           //
-}));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 var index = 0;
