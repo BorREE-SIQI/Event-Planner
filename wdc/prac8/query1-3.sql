dd5d49296ba45ef70ee4143d96637c3c8bbaa652
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-26 00:39:54 +0000

    Thu, May 26, 2022, 12:39 AM +00:00

---
+++ wdc/prac8/query1-3.sql
@@ -0,0 +1,1 @@
+SELECT customer.first_name,customer.last_name,rental.rental_date FROM customer INNER JOIN rental ON customer.customer_id = rental.customer_id where rental.return_date is NULL ORDER BY rental.rental_date LIMIT 1;
