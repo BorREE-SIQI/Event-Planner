fd1cd45257f6dc9355cd9277c70fa2322851351a
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-26 04:13:16 +0000

    Thu, May 26, 2022, 4:13 AM +00:00

--- wdc/prac8/app.js
+++ wdc/prac8/app.js
@@ -10,7 +10,7 @@ var mysql = require('mysql');
 // create a 'pool' (group) of connections to be used for connecting with our SQL server
 var dbConnectionPool = mysql.createPool({
     host: 'localhost',
-    database: 'blog'
+    database: 'sakila.sql'
   });
 
 var app = express();
