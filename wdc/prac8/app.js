d021a12eae3b0088550de0caab61c9f43b630fa7
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-26 03:55:47 +0000

    Thu, May 26, 2022, 3:55 AM +00:00

--- wdc/prac8/app.js
+++ wdc/prac8/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var mysql = require('mysql');
 // create a 'pool' (group) of connections to be used for connecting with our SQL server
 var dbConnectionPool = mysql.createPool({
@@ -9,10 +13,12 @@ var dbConnectionPool = mysql.createPool({
     database: 'blog'
   });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+
+app.use(function(req, res, next) {
+    req.pool = dbConnectionPool;
+    next();
+  });
 
 app.use(logger('dev'));
 app.use(express.json());
