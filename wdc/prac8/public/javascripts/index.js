55b35d34a6775809e970315a84de1bd1f0b1c9e9
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-26 03:41:54 +0000

    Thu, May 26, 2022, 3:41 AM +00:00

--- wdc/prac8/public/javascripts/index.js
+++ wdc/prac8/public/javascripts/index.js
@@ -1,13 +1,14 @@
 function add() {
 
   let actor = {
-    first_name: document.getElementsById()
-  }
+    first_name: document.getElementsById("actor-first-name").value,
+    last_name: document.getElementsById("actor-last-name").value,
+  };
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-
+        alert("Add Successful");
     }
   };
   loadTime.open("GET", "/last.txt");
