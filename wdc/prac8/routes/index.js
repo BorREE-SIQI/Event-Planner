fa498a398360d94fb790082c1a600f2a3987bcaa
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-26 09:13:55 +0000

    Thu, May 26, 2022, 9:13 AM +00:00

--- wdc/prac8/routes/index.js
+++ wdc/prac8/routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/newActor', function(req, res, next) {
-  var first_name = req.query.first_name; // if username was a field in the request URL
     var query = "INSERT INTO actor(first_name last_name) VALUES(?,?);";
     connection.query(query, [req.body.first_name, req.body.last_name], function(err, rows, fields) {
       connection.release(); // release connection
