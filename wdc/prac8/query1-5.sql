46c59a1062a51a2e0a0abf3a73e820f71a2a159c
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-26 00:59:35 +0000

    Thu, May 26, 2022, 12:59 AM +00:00

--- wdc/prac8/query1-5.sql
+++ wdc/prac8/query1-5.sql
@@ -1,4 +1,4 @@
 SELECT length FROM film
   WHERE film_id = (SELECT film_id FROM film_actor
                      WHERE actor_id = (SELECT actor_id FROM actor
-                     WHERE first_name = 'ANGELA' AND last_name = 'WITHERSPOON'));
+                        WHERE first_name = 'ANGELA' AND last_name = 'WITHERSPOON'));
