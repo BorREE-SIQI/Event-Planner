551a4faa717a019b11a23e9906baa124da4ff07a
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-05-27 03:26:33 +0000

    Fri, May 27, 2022, 3:26 AM +00:00

--- wdc/prac8/routes/index.js
+++ wdc/prac8/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/newActor', function(req, res, next) {
+    req.pool.getConnection(function(err, connection))
+    {
+      
+    }
     var query = "INSERT INTO actor(first_name last_name) VALUES(?,?);";
     connection.query(query, [req.body.first_name, req.body.last_name], function(err, rows, fields) {
       connection.release(); // release connection
