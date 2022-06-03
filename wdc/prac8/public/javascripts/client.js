ab45c321b35ccddeb27157804e1a95521840670f
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-03 05:50:10 +0000

    Fri, Jun 3, 2022, 5:50 AM +00:00

--- wdc/prac8/public/javascripts/client.js
+++ wdc/prac8/public/javascripts/client.js
@@ -5,7 +5,32 @@ var prac8 = new Vue({
       { first: 'first1',last: 'last1' },
       { first: 'first2',last: 'last2' }
     ],
-    method:
+
+  },
+  methods: {
+    add: function() {
+      let actor = {
+        first_name: document.getElementById("actor-first-name").value,
+        last_name: document.getElementById("actor-last-name").value
+      };
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Add Successful");
+        }
+      };
+      xhttp.open("POST", "/newActor");
+      // Set content type to JSON
+      xhttp.setRequestHeader("Content-type", "application/json");
+      // Send request
+      xhttp.send(JSON.stringify(actor));
+    },
+
+    show: function() {
+      
+    }
+
   }
 })
 
