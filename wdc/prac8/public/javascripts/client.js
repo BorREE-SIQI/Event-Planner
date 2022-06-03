0e2a8336a8121d1902f4baf7c1e3c9d75819b3d5
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-03 05:55:07 +0000

    Fri, Jun 3, 2022, 5:55 AM +00:00

--- wdc/prac8/public/javascripts/client.js
+++ wdc/prac8/public/javascripts/client.js
@@ -42,39 +42,39 @@ var prac8 = new Vue({
   }
 })
 
-function add() {
+// function add() {
 
-  let actor = {
-    first_name: document.getElementById("actor-first-name").value,
-    last_name: document.getElementById("actor-last-name").value
-  };
+//   let actor = {
+//     first_name: document.getElementById("actor-first-name").value,
+//     last_name: document.getElementById("actor-last-name").value
+//   };
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        alert("Add Successful");
-    }
-  };
-  xhttp.open("POST", "/newActor");
-  // Set content type to JSON
-  xhttp.setRequestHeader("Content-type", "application/json");
-  // Send request
-  xhttp.send(JSON.stringify(actor));
-  }
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         alert("Add Successful");
+//     }
+//   };
+//   xhttp.open("POST", "/newActor");
+//   // Set content type to JSON
+//   xhttp.setRequestHeader("Content-type", "application/json");
+//   // Send request
+//   xhttp.send(JSON.stringify(actor));
+//   }
 
 
-  function show() {
+//   function show() {
 
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        actors = this.response;
-        console.log(actors);
-      }
-    };
-    xhttp.open("GET", "/showActor");
-    xhttp.send();
-    }
+//       var xhttp = new XMLHttpRequest();
+//       xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//         actors = this.response;
+//         console.log(actors);
+//       }
+//     };
+//     xhttp.open("GET", "/showActor");
+//     xhttp.send();
+//     }
 
   //   var vueinst = new Vue({
   //     el: '#main',
