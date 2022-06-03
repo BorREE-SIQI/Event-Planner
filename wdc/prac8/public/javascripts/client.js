6dec50d74746af1dec5b6c9b057821f5ff627ffe
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-03 05:35:17 +0000

    Fri, Jun 3, 2022, 5:35 AM +00:00

--- wdc/prac8/public/javascripts/client.js
+++ wdc/prac8/public/javascripts/client.js
@@ -24,7 +24,8 @@ function add() {
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-
+        actors = this.response;
+        console.log(actors);
       }
     };
     xhttp.open("GET", "/showActor");
