0137931c5934d8fe74ee1384f49ea4c756fd0d1b
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-07 04:43:41 +0000

    Tue, Jun 7, 2022, 4:43 AM +00:00

--- wdc/wdc_project/myDatabase.sql
+++ wdc/wdc_project/myDatabase.sql
@@ -9,11 +9,11 @@ CREATE TABLE admin (
 );
 
 CREATE TABLE users (
-    users_email VARCHAR(127) UNIQUE,
+    email VARCHAR(127) UNIQUE,
     password VARCHAR(127),
     last_name VARCHAR(63),
     first_name VARCHAR(63),
-    PRIMARY KEY (users_email)
+    PRIMARY KEY (email)
 );
 
 CREATE TABLE events (
@@ -26,7 +26,7 @@ CREATE TABLE events (
 CREATE TABLE creating_trip (
     users_email VARCHAR(127),
     event_ID INT,
-    FOREIGN KEY (users_email) REFERENCES users(users_email) ON DELETE SET NULL,
+    FOREIGN KEY (users_email) REFERENCES users(email) ON DELETE SET NULL,
     FOREIGN KEY (event_ID) REFERENCES events(ID) ON DELETE SET NULL
 );
 
