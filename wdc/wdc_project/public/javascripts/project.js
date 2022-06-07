45e825aba1a2bcdb03dcfcdf306dfd3f5ebff53f
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-07 04:49:36 +0000

    Tue, Jun 7, 2022, 4:49 AM +00:00

---
+++ wdc/wdc_project/public/javascripts/project.js
@@ -0,0 +1,18 @@
+function showUser()
+{
+    let users = {
+        users_email: document.getElementById("search").value
+      };
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("search Successful");
+        }
+      };
+      xhttp.open("POST", "/manageUser.html");
+      // Set content type to JSON
+      xhttp.setRequestHeader("Content-type", "application/json");
+      // Send request
+      xhttp.send(JSON.stringify(users));
+}
