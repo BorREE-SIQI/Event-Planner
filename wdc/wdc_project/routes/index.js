9bf964ea6d5fd2a1194cec73f97caecf4752f74d
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-07 14:44:36 +0000

    Tue, Jun 7, 2022, 2:44 PM +00:00

--- wdc/wdc_project/routes/index.js
+++ wdc/wdc_project/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/searchUsers', function (req, res, next) {
+router.post('/searchUser', function (req, res, next) {
   //Connect to the database
   req.pool.getConnection( function(err,connection) {
     if (err) {
@@ -54,8 +54,30 @@ router.post('/manageUsers', function (req, res, next) {
       return;
     }
 
-    var query = "UPDATE events SET event_name = ? description = ? WHERE email = ?;";
-    connection.query(query, [req.body.event,req.body.d,req.body.email], function (err, rows, fields) {
+    var query = "UPDATE users SET password = ? first_name = ? last_name = ?;";
+    connection.query(query, [req.body.password,req.body.password,req.body.first_name,req.body.last_name], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  });
+});
+
+router.post('/manageEvents', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "UPDATE events SET event_name = ? description = ?;";
+    connection.query(query, [req.body.event,req.body.d], function (err, rows, fields) {
       connection.release(); // release connection
       if (err) {
         res.sendStatus(500);
