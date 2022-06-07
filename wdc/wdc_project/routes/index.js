c77e09bb181550d68cacc2c83e967e1b4d26f117
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-07 09:41:41 +0000

    Tue, Jun 7, 2022, 9:41 AM +00:00

--- wdc/wdc_project/routes/index.js
+++ wdc/wdc_project/routes/index.js
@@ -22,4 +22,26 @@ router.post('/manageUser', function (req, res, next) {
     });
   });
 });
+
+router.post('/manageEvent', function (req, res, next) {
+  //Connect to the database
+  req.pool.getConnection( function(err,connection) {
+    if (err) {
+      res.sendStatus(500);
+      console.log(err);
+      return;
+    }
+
+    var query = "SELECT email password first_name last_name FROM users WHERE email = ?;";
+    connection.query(query, [req.body.email], function (err, rows, fields) {
+      connection.release(); // release connection
+      if (err) {
+        res.sendStatus(500);
+        console.log(err);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  });
+});
 module.exports = router;
