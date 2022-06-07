ac539e612a7378fc4cd9764cb578c1910df7fa67
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-07 09:51:49 +0000

    Tue, Jun 7, 2022, 9:51 AM +00:00

--- wdc/wdc_project/public/javascripts/project.js
+++ wdc/wdc_project/public/javascripts/project.js
@@ -8,6 +8,10 @@ function showUser()
         xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             alert("search Successful");
+            document.getElementsByName("input-email") = this.response.email;
+            document.getElementsByName("input-password") = this.response.password;
+            document.getElementsByName("input-last") = this.response.last_name;
+            document.getElementsByName("input-first") = this.response.first_name;
         }
       };
       xhttp.open("POST", "/manageUser");
@@ -28,6 +32,8 @@ function showEvent()
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         alert("search Successful");
+        document.getElementsByName("input-event") = this.response.event_name;
+        document.getElementsByName("input-d") = this.response.description;
     }
   };
   xhttp.open("POST", "/manageEvent");
@@ -35,4 +41,9 @@ function showEvent()
   xhttp.setRequestHeader("Content-type", "application/json");
   // Send request
   xhttp.send(JSON.stringify(users));
+}
+
+function modifyUser()
+{
+  
 }
