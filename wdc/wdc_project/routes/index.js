c9a0314df5cad4ec175849a49113b8492142cc27
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-08 07:52:33 +0000

    Wed, Jun 8, 2022, 7:52 AM +00:00

--- wdc/wdc_project/routes/index.js
+++ wdc/wdc_project/routes/index.js
@@ -56,7 +56,7 @@ router.post('/manageUsers', function (req, res, next) {
       return;
     }
 
-    var query = "UPDATE users SET password = ? first_name = ? last_name = ? WHERE email = ?;";
+    var query = "UPDATE users SET password = ?, first_name = ?, last_name = ? WHERE email = ?;";
     connection.query(query, [req.body.password,req.body.first_name,req.body.last_name,req.body.email], function (err, rows, fields) {
       connection.release(); // release connection
       if (err) {
