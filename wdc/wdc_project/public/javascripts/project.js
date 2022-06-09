e720bc4bfa9ca4308c9d7cdcc7db60980b0f3438
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-09 11:13:47 +0000

    Thu, Jun 9, 2022, 11:13 AM +00:00

--- wdc/wdc_project/public/javascripts/project.js
+++ wdc/wdc_project/public/javascripts/project.js
@@ -182,6 +182,8 @@ function pickTime()
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      var obj = JSON.parse(this.response);
+      document.getElementsByName("finalDate") = obj
         alert("Add Successful");
     }
   };
