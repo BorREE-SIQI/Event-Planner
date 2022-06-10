933ebb700cf9ab6da90bc5129f8ecb096d474061
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-10 09:14:20 +0000

    Fri, Jun 10, 2022, 9:14 AM +00:00

--- wdc/wdc_project/public/javascripts/client.js
+++ wdc/wdc_project/public/javascripts/client.js
@@ -244,6 +244,7 @@ function userEvent()
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      console.log(this.response);
         var obj = JSON.parse(this.response);
         console.log(obj);
         alert("Show Successful");
