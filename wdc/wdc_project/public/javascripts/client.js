ef735e393cfa44a097e269a5cdf56f08a1aa56d3
Coucou <71242422+BorREE-SIQI@users.noreply.github.com>
2022-06-10 13:46:38 +0000

    Fri, Jun 10, 2022, 1:46 PM +00:00

--- wdc/wdc_project/public/javascripts/client.js
+++ wdc/wdc_project/public/javascripts/client.js
@@ -1,18 +1,211 @@
-var project = new Vue({
+// var project = new Vue({
 
-  el: '#vue',
-  data: {
-    search_user : ''
-  },
+//   el: '#vue',
+//   data: {
+//     search_user : ''
+//   },
 
-  methods: {
+//   methods: {
 
-showUser: function()
+// showUser: function()
+// {
+//     let users = {
+//         email: project.search_user
+//         //document.getElementsByName("userSearch")[0].value
+//       };
+
+//         var xhttp = new XMLHttpRequest();
+//         xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             var obj = JSON.parse(this.response);
+//             console.log(obj);
+//             alert("search Successful");
+//             window.location.replace(`./manageUser.html?email=${obj.email}&last_name=${obj.last_name}&first_name=${obj.first_name}&password=${obj.password}`);
+//         }
+//       };
+//       xhttp.open("POST", "/searchUsers");
+//       // Set content type to JSON
+//       xhttp.setRequestHeader("Content-type", "application/json");
+//       // Send request
+//       xhttp.send(JSON.stringify(users));
+// }
+//   }
+
+// })
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          isGoogleLogIn= true
+            alert("Login Successfully");
+           window.location.replace("home.html");
+        }else if (this.readyState == 4 && this.status >= 400){
+            alert("Login Failed");
+        }
+    };
+
+    xhttp.open("POST", "/login");
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({token : googleUser.getAuthResponse().id_token}));
+
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  function signOut() {
+
+  try {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('Signed out of Google');
+     });
+
+    } catch (error){}
+
+    let xhttp = new XMLHttpRequest();
+
+     xhttp.onreadystatechange = function () {
+           if (this.readyState == 4 && this.status == 200) {
+              alert("User Successully Signed Out");
+             window.location.replace("index.html");
+          }else if (this.readyState == 4 && this.status >= 400){
+              alert("User Signed Out Failed");
+          }
+      };
+
+      xhttp.open("POST", "/logout");
+
+      xhttp.send();
+
+    }
+
+
+////////////////////////////////////////////////////////////////////////////////
+function login() {
+
+    console.log("inside login()");
+
+    let user={
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value,
+        //token: localStorage.getItem('token')
+    };
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Login Successfully");
+           window.location.replace("home.html");
+        }else if (this.readyState == 4 && this.status >= 400){
+            alert("Login Failed");
+        }
+    };
+
+    xhttp.open("POST", "/login",true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(user));
+
+  }
+
+  function onSignIn2(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Login Successfully");
+           window.location.replace("homeAdmin.html");
+        }else if (this.readyState == 4 && this.status >= 400){
+            alert("Login Failed");
+        }
+    };
+
+    xhttp.open("POST", "/adminLogin");
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({token : googleUser.getAuthResponse().id_token}));
+
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+  function adminLogin() {
+
+    console.log("inside login()");
+
+    let user={
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value,
+        //token: localStorage.getItem('token')
+    };
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Login Successfully");
+           window.location.replace("homeAdmin.html");
+        }else if (this.readyState == 4 && this.status >= 400){
+            alert("Login Failed");
+        }
+    };
+
+    xhttp.open("POST", "/adminLogin",true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(user));
+
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+
+  function createAccount() {
+
+    // console.log("inside login()");
+
+    let user={
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value,
+        first_name:document.getElementById('first_name').value,
+        last_name:document.getElementById('last_name').value,
+        //token: localStorage.getItem('token')
+    };
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Account Created Successfully");
+           window.location.replace("home.html");
+        }else if (this.readyState == 4 && this.status >= 400){
+            alert("Failed to Create Account");
+        }
+    };
+
+    xhttp.open("POST", "/userAccount",true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(user));
+
+  }
+
+
+
+  function showUser()
 {
     let users = {
-        email: project.search_user
-        //document.getElementsByName("userSearch")[0].value
+        email: document.getElementsByName("userSearch")[0].value
       };
+
+      console.log(users);
 
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
@@ -29,9 +222,6 @@ showUser: function()
       // Send request
       xhttp.send(JSON.stringify(users));
 }
-  }
-
-});
 
 
 function showEvent()
@@ -46,7 +236,7 @@ function showEvent()
         var obj = JSON.parse(this.response);
         console.log(obj);
         alert("search Successful");
-        window.location.replace(`./manageEvent.html?event_name=${obj.event_name}&description=${obj.description}&ID=${obj.ID}`);
+        window.location.replace(`./manageEvent.html?event_name=${obj.event_name}&description=${obj.description}`);
 
     }
   };
@@ -112,7 +302,6 @@ function loadEvent()
 {
   const urlParams = new URLSearchParams(window.location.search);
   document.getElementsByName("input-event")[0].value = urlParams.get('event_name');
-  document.getElementsByName("input-time")[0].value = urlParams.get('ID');
   document.getElementsByName("input-d")[0].value = urlParams.get('description');
 }
 
@@ -267,4 +456,24 @@ function loadUserEvent()
   document.getElementsByName("user-event-name")[0].value = urlParams.get('event_name');
   document.getElementsByName("user-event-ID")[0].value = urlParams.get('ID');
   document.getElementsByName("user-event-d")[0].value = urlParams.get('description');
+}
+
+
+function loadProfile()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.response);
+      var obj = JSON.parse(this.response);
+      console.log(obj);
+      document.getElementsByName("profile-email")[0].value= obj.email;
+      document.getElementsByName("profile-password")[0].value= obj.password;
+      document.getElementsByName("profile-first")[0].value= obj.first_name;
+      document.getElementsByName("profile-last")[0].value= obj.last_name;
+        //alert("Add Successful");
+    }
+  };
+  xhttp.open("GET", "/profile");
+  xhttp.send();
 }
--- wdc/wdc_project/routes/index.js
+++ wdc/wdc_project/routes/index.js
@@ -1,6 +1,320 @@
 var express = require('express');
 var router = express.Router();
-const sanitizeHtml = require('sanitize-html');
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client("1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'landingPage' });
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
+router.post('/login', function(req, res, next) {
+
+  // console.log("inside route")
+
+  if ('email' in req.body && 'password' in req.body) {
+
+      req.pool.getConnection(function(error,connection){
+        if(error){
+          console.log("connection error")
+          console.log(error);
+          res.sendStatus(500);
+          return;
+        }
+
+        let query = "SELECT first_name,last_name, email FROM users WHERE email = ? AND password = ?;";
+        connection.query(query,[req.body.email,req.body.password],function(error, rows, fields) {
+          connection.release(); // release connection
+          if (error) {
+            console.log("query error")
+            console.log(error);
+            res.sendStatus(500);
+            return;
+          }
+
+          if (rows.length > 0){
+            console.log('success');
+            req.session.user = req.body.email;
+            res.sendStatus(200);
+          }else{
+            console.log('bad login');
+            res.sendStatus(401);
+          }
+
+          });
+        });
+
+  }else if ('token' in req.body) {
+
+        let email =null;
+        async function verify() {
+          const ticket = await client.verifyIdToken({
+              idToken: req.body.token,
+              audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+              // Or, if multiple clients access the backend:
+              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+          });
+          const payload = ticket.getPayload();
+          const userid = payload['sub'];
+          console.log(userid)
+          email = payload['email'];
+          // email = payload['email'];
+        }
+        // verify().then(function).catch(console.error);//////
+        verify().then(function(){
+
+          req.pool.getConnection(function(error,connection){
+            if(error){
+              console.log("connection error")
+              console.log(error);
+              res.sendStatus(500);
+              return;
+            }
+
+            let query = "SELECT first_name,last_name,email FROM users WHERE email = ?;";
+            connection.query(query,[email],function(error, rows, fields) {
+              connection.release(); // release connection
+              if (error) {
+                console.log("query error")
+                console.log(error);
+                res.sendStatus(500);
+                return;
+              }
+
+              if (rows.length > 0){
+                console.log('success');
+                req.session.user = req.body.email;
+                res.sendStatus(200);
+              }else{
+                console.log('bad login');
+                res.sendStatus(401);
+              }
+
+              });
+            });
+          }).catch(function(){
+          res.sendStatus(401);
+        });
+
+  } else {
+    console.log('bad request');
+    res.sendStatus(400);
+  }
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+router.get('/test', function(req, res, next) {
+  if('user' in req.session){
+    res.json(req.session.user);
+  } else {
+    res.send('This is a test');
+  }
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
+router.post('/adminLogin', function(req, res, next) {
+
+  if ('email' in req.body && 'password' in req.body) {
+
+      req.pool.getConnection(function(error,connection){
+        if(error){
+          console.log("connection error")
+          console.log(error);
+          res.sendStatus(500);
+          return;
+        }
+
+        let query = "SELECT email FROM admin WHERE email = ? AND password = ?;";
+        connection.query(query,[req.body.email,req.body.password],function(error, rows, fields) {
+          connection.release(); // release connection
+          if (error) {
+            console.log("query error")
+            console.log(error);
+            res.sendStatus(500);
+            return;
+          }
+
+          if (rows.length > 0){
+            console.log('success');
+            req.session.user = req.body.email;
+            res.sendStatus(200);
+          }else{
+            console.log('bad login');
+            res.sendStatus(401);
+          }
+
+          });
+        });
+
+  }else if ('token' in req.body) {
+
+        let email =null;
+        async function verify() {
+          const ticket = await client.verifyIdToken({
+              idToken: req.body.token,
+              audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+              // Or, if multiple clients access the backend:
+              //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+          });
+          const payload = ticket.getPayload();
+          const userid = payload['sub'];
+          console.log(userid)
+          email = payload['email'];
+          // email = payload['email'];
+        }
+        // verify().then(function).catch(console.error);//////
+        verify().then(function(){
+
+          req.pool.getConnection(function(error,connection){
+            if(error){
+              console.log("connection error")
+              console.log(error);
+              res.sendStatus(500);
+              return;
+            }
+
+            let query = "SELECT email FROM admin WHERE email = ?;";
+            connection.query(query,[email],function(error, rows, fields) {
+              connection.release(); // release connection
+              if (error) {
+                console.log("query error")
+                console.log(error);
+                res.sendStatus(500);
+                return;
+              }
+
+              if (rows.length > 0){
+                console.log('success');
+                req.session.user = req.body.email;
+                res.sendStatus(200);
+              }else{
+                console.log('bad login');
+                res.sendStatus(401);
+              }
+
+              });
+            });
+          }).catch(function(){
+          res.sendStatus(401);
+        });
+
+  } else {
+    console.log('bad request');
+    res.sendStatus(400);
+  }
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+
+router.post('/userAccount', function(req, res, next) {
+
+  if ('email' in req.body && 'password' in req.body) {
+
+    console.log("in the route")
+
+      req.pool.getConnection(function(error,connection){
+        if(error){
+          console.log("connection error")
+          console.log(error);
+          res.sendStatus(500);
+          return;
+        }
+
+        console.log("connected to database")
+        let query = "INSERT INTO users VALUES (?,?,?,?);";
+        connection.query(query,[req.body.email, req.body.password, req.body.last_name, req.body.first_name],function(error, rows, fields) {
+
+          connection.release(); // release connection
+          if (error) {
+            console.log("query error")
+            console.log(error);
+            res.sendStatus(500);
+            return;
+          }
+           req.session.user = req.body.email;
+            res.sendStatus(200);
+
+          });
+        });
+
+  // } //else if ('token' in req.body) {
+
+  //       let email =null;
+  //       async function verify() {
+  //         const ticket = await client.verifyIdToken({
+  //             idToken: req.body.token,
+  //             audience: '1006345318538-06u5fqd3sur2bthaea307jtjalc33f3r.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+  //             // Or, if multiple clients access the backend:
+  //             //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  //         });
+  //         const payload = ticket.getPayload();
+  //         const userid = payload['sub'];
+  //         console.log(userid)
+  //         email = payload['email'];
+  //         // email = payload['email'];
+  //       }
+  //       // verify().then(function).catch(console.error);//////
+  //       verify().then(function(){
+
+  //         req.pool.getConnection(function(error,connection){
+  //           if(error){
+  //             console.log("connection error")
+  //             console.log(error);
+  //             res.sendStatus(500);
+  //             return;
+  //           }
+
+  //           let query = "SELECT email FROM admin WHERE email = ?;";
+  //           connection.query(query,[email],function(error, rows, fields) {
+  //             connection.release(); // release connection
+  //             if (error) {
+  //               console.log("query error")
+  //               console.log(error);
+  //               res.sendStatus(500);
+  //               return;
+  //             }
+
+  //             if (rows.length > 0){
+  //               console.log('success');
+  //               //req.session.user = row[0];
+  //               res.sendStatus(200);
+  //             }else{
+  //               console.log('bad login');
+  //               res.sendStatus(401);
+  //             }
+
+  //             });
+  //           });
+  //         }).catch(function(){
+  //         res.sendStatus(401);
+  //       });
+
+  } else {
+    console.log('bad request because no email and password');
+    res.sendStatus(400);
+  }
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+router.post('/logout', function(req, res, next) {
+  console.log("log out routes");
+
+  if('user' in req.session){
+     delete req.session.user;
+   // req.session.destroy();
+  }
+  res.end();
+
+});
+
+
+
 
 router.post('/searchUsers', function (req, res, next) {
   //Connect to the database
@@ -58,7 +372,7 @@ router.post('/manageUsers', function (req, res, next) {
     }
 
     var query = "UPDATE users SET password = ?, first_name = ?, last_name = ? WHERE email = ?;";
-    connection.query(query, [req.body.password,sanitizeHtml(req.body.first_name),sanitizeHtml(req.body.last_name),req.body.email], function (err, rows, fields) {
+    connection.query(query, [req.body.password,req.body.first_name,req.body.last_name,req.body.email], function (err, rows, fields) {
       connection.release(); // release connection
       if (err) {
         res.sendStatus(500);
@@ -229,29 +543,6 @@ router.post('/createEvents', function (req, res, next) {
   });
 });
 
-router.get('/profile', function (req, res, next) {
-  //Connect to the database
-  req.pool.getConnection( function(err,connection) {
-    if (err) {
-      res.sendStatus(500);
-      console.log(err);
-      return;
-    }
-
-    var query = "SELECT * FROM users WHERE password = ?, first_name = ?, last_name = ? WHERE email = ?;";
-
-    connection.query(query, [req.query.password,req.query.first_name,req.query.last_name,req.query.email], function (err, rows, fields) {
-      connection.release(); // release connection
-      if (err) {
-        res.sendStatus(500);
-        console.log(err);
-        return;
-      }
-      res.sendStatus(200);
-    });
-  });
-});
-
 router.post('/userEvents', function (req, res, next) {
   //Connect to the database
   req.pool.getConnection( function(err,connection) {
@@ -274,4 +565,36 @@ router.post('/userEvents', function (req, res, next) {
   });
 });
 
+
+router.get('/profile', (req, res, next) => {
+  if(!('user' in req.session))
+  {
+    res.sendstatus(500);
+    alert("user has not logged");
+    return;
+  }
+  else
+  {
+  email = req.session.user;
+  console.log(email);
+  req.pool.getConnection( function(err,connection) {
+         if (err) {
+           res.sendStatus(500);
+           return;
+         }
+         var query = "SELECT * FROM users WHERE email = '"+email+"';";
+         connection.query(query, function(err, rows, fields) {
+         connection.release(); // release connection
+
+       //   if (err)
+       //   {
+       //     res.send('1');
+       //   }
+           res.json(rows[0]);
+         });
+     });
+  }
+});
+
 module.exports = router;
+
